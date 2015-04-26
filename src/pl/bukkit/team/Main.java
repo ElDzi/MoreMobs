798cd29b0f6ae29e34af3f0fba576c7e90c0837a
Zahusek <zahusek@gmail.com>
2015-04-26 13:02:38 +0200

    Main.java - v0.0

---
+++ src/pl/bukkit/team/Main.java
@@ -0,0 +1,16 @@
+package pl.bukkit.team;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main1 extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+}
