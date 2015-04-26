b9ff9009a001d8ab6ca6bc476719cc57fbdd18dc
Zahusek <zahusek@gmail.com>
2015-04-26 14:25:21 +0200

    PacketPlayOutPlayerInfo.java - v1.0

---
+++ src/pl/bukkit/team/packets/PacketPlayOutPlayerInfo.java
@@ -0,0 +1,27 @@
+package pl.bukkit.team.packets;
+
+import pl.bukkit.team.Main;
+import pl.bukkit.team.protocols.Reflection;
+import pl.bukkit.team.protocols.TinyProtocol;
+
+public class PacketPlayOutPlayerInfo extends AbstractPacket {
+	
+	private static final Object handle = Reflection.getConstructor("{nms}.PacketPlayOutPlayerInfo").invoke();
+	
+	public PacketPlayOutPlayerInfo() { super(handle, Main.getTinyProtocol());}
+    
+    public PacketPlayOutPlayerInfo(Object packet,TinyProtocol tinyprotocol) { super(packet, tinyprotocol); }
+
+	
+    public String getPlayerName() { return (String) Reflection.getMethod(packet.getClass(), "a").invoke(packet); }
+    
+    public void setPlayerName(String value) { Reflection.getField(packet.getClass(), "a", String.class).set(packet, value); }
+    
+    public boolean getOnline() { return (boolean) Reflection.getMethod(packet.getClass(), "b").invoke(packet); }
+    
+    public void setOnline(boolean value) { Reflection.getField(packet.getClass(), "b", boolean.class).set(packet, value); }
+    
+    public int getPing() { return (int) Reflection.getMethod(packet.getClass(), "c").invoke(packet); }
+    
+    public void setPing(int value) { Reflection.getField(packet.getClass(), "c", int.class).set(packet, value); }
+}
